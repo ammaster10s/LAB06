94922208a589912e20868473b5173ea28a0f1d18
ammaster10s <pawaris_avira@hotmail.com>
2023-03-13 09:34:56 +0700

    Bhoon

---
+++ BarPanel.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+
+//Inherit appropriate superclass 
+public class BarPanel {
+	public BarPanel() {
+		//Create controlpanel and textfield_ChVolDisplay
+		
+		//Add to this BarPanel panel
+
+	}
